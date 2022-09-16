82430e876d0332eb1f6e263b0634cc66348b661f
Vamsi Bellamkonda <vb66919@gmail.com>
2022-09-16 11:14:21 +0200

    add demo project to local repository

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class TestController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Welcome to Demo";
+	}
+}
